9f3220f5dfebdb224f43c0502c5cf0e941c82fdb
~zw <zeppelinwave@gmail.com>
2018-07-11 21:49:11 +0100

    parse_options() changed to return by value

--- src/munch.h
+++ src/munch.h
@@ -1,6 +1,7 @@
 #ifndef MUNCH_H_
 #define MUNCH_H_
 namespace munch{
+  /* Holds options parsed from command line arguments */
   struct munch_options {
     std::string database = "~/munch.sqlite3";
   };
